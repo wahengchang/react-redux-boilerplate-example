d543a8664e6b29aeaa25e478888634c7c89da499
waheng <peterchang@yahoo-inc.com>
2017-06-13 09:45:08 +0800

    Presentational Component: Topic Create

---
+++ src/TopicCreate.js
@@ -0,0 +1,52 @@
+import React, { PropTypes } from 'react'
+
+class TopicListRow extends React.Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         data: 'Initial data...'
+      }
+      this.updateStateData = this.updateStateData.bind(this);
+   };
+
+   updateStateData(val) {
+      this.setState({data: val})
+   }
+
+  render() {
+    const { createTopic } = this.props
+    let { data } = this.state
+    
+    const onChange = (e) => {
+        console.log('e.target.value: ', e.target.value);
+        this.updateStateData( e.target.value)
+    }
+
+    const _createTopic = () => {
+        createTopic(data)
+        updateStateData('')
+    }
+
+    return (<div>
+            <label>
+                content
+            </label>
+            <input 
+                required
+                onChange={onChange}  
+                value={data}  
+                type="text"
+                placeholder="your  content"/> 
+            <button onClick={_createTopic}> Add</button>
+            <span>
+                {this.state.data}
+            </span>
+        </div>)
+  }
+}
+
+TopicListRow.propTypes = {
+  createTopic: PropTypes.func.isRequired,
+}
+
+export default TopicListRow
